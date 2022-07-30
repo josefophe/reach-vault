1ae9f12bb40dccd144eec44a76fe1a10aca2071e
Joseph Ebin <josefophe@gmail.com>
2022-07-30 18:09:07 +0100

    reach vault

--- index.mjs
+++ index.mjs
@@ -4,24 +4,58 @@ const stdlib = loadStdlib(process.env);
 
 const startingBalance = stdlib.parseCurrency(100);
 
-const [ accAlice, accBob ] =
-  await stdlib.newTestAccounts(2, startingBalance);
+/* const [ accAlice, accBob ] =
+  await stdlib.newTestAccounts(2, startingBalance); */
+
+const accAlice = await stdlib.newTestAccount(stdlib.parseCurrency(6000))
+const accBob = await stdlib.newTestAccount(startingBalance)
+
 console.log('Hello, Alice and Bob!');
 
 console.log('Launching...');
+
 const ctcAlice = accAlice.contract(backend);
 const ctcBob = accBob.contract(backend, ctcAlice.getInfo());
+
+const shared = () =>({
+  showTime: (t) =>{
+    console.log(`Time is ${t}`)
+  }
+})
+
+const choiceArray = ["I’m not here", "I’m still here”"]
+
+const getAccountBalance = async (who) => stdlib.balanceOf(who)
+
+console.log(`Starting account balance of Alice is ${await getAccountBalance(accAlice)}`)
+console.log(`Starting account balance of Bob is ${await getAccountBalance(accBob)}`)
 
 console.log('Starting backends...');
 await Promise.all([
   backend.Alice(ctcAlice, {
-    ...stdlib.hasRandom,
     // implement Alice's interact object here
+    ...stdlib.hasRandom,
+    ...shared,
+    inherit: stdlib.parseCurrency(5000),
+    getChoice: () => {
+     const choice = Math.floor(Math.random() * 2)
+     console.log(`Alice choice is ${choiceArray[choice]}`)
+      return choice == 0 ? false : true
+    }
   }),
+
   backend.Bob(ctcBob, {
     ...stdlib.hasRandom,
     // implement Bob's interact object here
+    ...shared,
+    acceptTerms: (amount) =>{
+      console.log(`Bob accept the terms of The Vault for  ${stdlib.parseCurrency(amount)}`)
+      return true
+    }
   }),
 ]);
 
-console.log('Goodbye, Alice and Bob!');
+console.log(`Alice's balance after is: ${await getAccountBalance(accAlice)}`)
+console.log(`Bob's account balance after is ${await getAccountBalance(accBob)}`)
+
+console.log('Bye, Alice and Bob!');
--- index.rsh
+++ index.rsh
@@ -1,19 +1,55 @@
 'reach 0.1';
 
+// import { inherits } from "util";
+
+const countdown = 20;
+
+const shared = {
+  showTime: Fun([UInt], Null)
+}
+
 export const main = Reach.App(() => {
   const A = Participant('Alice', {
+    ...shared,
+    inherit: UInt,
+    getChoice: Fun([], Bool)
     // Specify Alice's interact interface here
   });
   const B = Participant('Bob', {
     // Specify Bob's interact interface here
+    ...shared,
+    acceptTerms: Fun([UInt], Bool)
   });
   init();
+  A.only(() => {
+    const amt = declassify(interact.inherit)
+  })
   // The first one to publish deploys the contract
-  A.publish();
+  A.publish(amt)
+    .pay(amt);
   commit();
+
+  B.only(() => {
+    const terms = declassify(interact.acceptTerms(amt))
+  })
   // The second one to publish always attaches
-  B.publish();
+  B.publish(terms);
   commit();
   // write your program here
+  each([A, B], () => {
+    interact.showTime(countdown);
+});
+  A.only(() => {
+    const stillHere = declassify(interact.getChoice())
+  })
+  A.publish(stillHere)
+  if (stillHere == true) {
+    transfer(amt).to(A);
+  } else {
+    transfer(amt).to(B);
+  }
+
+  commit()
+
   exit();
 });
